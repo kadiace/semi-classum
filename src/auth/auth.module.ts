22d1909a7449c70dc65e1dac2d2adad96df162f5
kadiace <rlacjfghks95@gmail.com>
2021-12-24 03:51:18 +0900

    success to get auth token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport'
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [ UserModule, PassportModule ],
-  providers: [ AuthService, LocalStrategy ]
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    })
+  ],
+  providers: [ AuthService, LocalStrategy ],
+  exports: [ AuthService ]
 })
 export class AuthModule {}
